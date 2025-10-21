3affa6c1931229474001be9556c584b8fd2fe0f9
Roberta Vitoria <roberta.vaibrilhar@gmail.com>
2025-10-21 00:26:30 -0300

    Primeiro commit

---
+++ src/persons.js
@@ -0,0 +1,28 @@
+const persons = [
+  {
+    name: "Guilherme",
+    nickname: "Gui",
+    age: 30,
+    email: "guilherme@example.com",
+  },
+  {
+    name: "laura silva",
+    nickname: "laurinha",
+    age: 60,
+    email: "laura@example.com",
+  },
+  {
+    name: "sophia leal",
+    nickname: "leal",
+    age: 16,
+    email: "sophia@example.com",
+  },
+  {
+    name: "armando souza",
+    nickname: "Armando",
+    age: 20,
+    email: "armando@example.com",
+  },
+];
+
+export { persons };
