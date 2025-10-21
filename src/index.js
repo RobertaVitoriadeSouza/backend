7b3d3e34d04b4f16bf00dbe2191fa815a61d9ccb
Roberta Vitoria <roberta.vaibrilhar@gmail.com>
2025-10-21 01:08:37 -0300

    salvando

--- src/index.js
+++ src/index.js
@@ -6,9 +6,18 @@ const app = express()
 const port = 3333
 
 app.use(cors())
+app.use(express.json())
 
 app.get("/", (request, response) => {
   response.json(persons)
+})
+app.post("/cadastrar", (request, response) => {
+  const { user } = request.body
+
+  console.log(user)
+  response.status(201).json({ message: "Usuário cadastrado com sucesso!" })
+  
+  
 })
 
 app.listen(port, () => {
